843af0e0d8db4bbc6d8ff4395a01db5154558b29
KarenVentura <ventura.802@hotmail.com>
2015-07-13 10:45:06 -0500

    Ejercicio Temperatura Resuelto

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -1,2 +1,35 @@
 class Temperature
+
+    def initialize(param)
+    @param  = param
+    end	
+    
+
+    def self.in_fahrenheit(param)
+	obj_fahren = Temperature.new(param)
+    end
+
+
+    def to_far
+    	
+     f = (@param*1.8000)+32
+     
+    end
+  
+
+	
+    def self.in_celsius(param)
+        obj_celsius = Temperature.new(param)
+    end
+
+
+    def to_cel
+         
+         c = (@param-32)/1.8000 
+      	    
+    end
+
+	
+     
+
 end
