925505564eb64c18d0a137d7b25b9ac02770e119
KarenVentura <ventura.802@hotmail.com>
2015-07-14 16:29:15 -0500

    Temp

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -1,35 +1,21 @@
+# Convercion de Temperauras
 class Temperature
-
-    def initialize(param)
+  def initialize(param)
     @param  = param
-    end	
-    
+  end
 
-    def self.in_fahrenheit(param)
-	obj_fahren = Temperature.new(param)
-    end
+  def self.in_fahrenheit(param)
+    Temperature.new(param)
+  end
 
+  def to_far
+    (@param * 1.8000) + 32
+  end
 
-    def to_far
-    	
-     f = (@param*1.8000)+32
-     
-    end
-  
-
-	
-    def self.in_celsius(param)
-        obj_celsius = Temperature.new(param)
-    end
-
-
-    def to_cel
-         
-         c = (@param-32)/1.8000 
-      	    
-    end
-
-	
-     
-
+  def self.in_celsius(param)
+    Temperature.new(param)
+  end
+  def to_cel
+    (@param - 32) / 1.8000
+  end
 end
